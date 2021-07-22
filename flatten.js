b7b78e9beee75b5654d74c01e4129f3d7cd8326c
Brian LaFond <brian.d.lafond@gmail.com>
2021-07-22 18:26:33 +0000

    modified

---
+++ flatten.js
@@ -0,0 +1,33 @@
+const eqArrays = function(array1, array2) {
+  
+  if (array1.length !== array2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  
+  let isEqual = eqArrays(actual, expected);
+
+  if (isEqual) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+const flatten = function(array) {
+
+  return array.flat();
+
+};
+
+console.log(flatten([1, 2, [3, 4], 5, [6]])) // => [1, 2, 3, 4, 5, 6]
