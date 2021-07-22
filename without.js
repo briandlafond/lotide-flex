0598b9dd5ea3fac0c8edc22a755faaf320d98c6c
Brian LaFond <brian.d.lafond@gmail.com>
2021-07-22 16:08:38 +0000

    modified

--- without.js
+++ without.js
@@ -16,4 +16,3 @@ const without = function(source, itemsToRemove) {
 console.log(without([1, 2, 3], [1])) // => [2, 3]
 console.log(without(["1", "2", "3"], [1, 2, "3"])) // => ["1", "2"]
 
-//why log the correct answer but not return the right answer?
