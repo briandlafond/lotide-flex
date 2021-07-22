ae1da52deba41ec01918d756a94e5cf5c9484fbd
Brian LaFond <brian.d.lafond@gmail.com>
2021-07-22 19:20:53 +0000

    modified to use includes method

--- without.js
+++ without.js
@@ -1,18 +1,49 @@
-const without = function(source, itemsToRemove) {
+const eqArrays = function(array1, array2) {
+  
+  if (array1.length !== array2.length) {
+    return false;
+  }
 
-  for (let i = 0; i < source.length; i++) {
-    for(let j = 0; j < itemsToRemove.length; j++) {
-      if (source[i] === itemsToRemove[j]) {
-        source.splice(i, 1);
-      }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
     }
   }
-  return source;
+  return true;
 };
 
+const assertArraysEqual = function(actual, expected) {
+  
+  let isEqual = eqArrays(actual, expected);
+
+  if (isEqual) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+const without = function(source, itemsToRemove) {
+
+  const withoutArray = [];
+
+  for (let i = 0; i < source.length; i++) {
+ 
+      if (itemsToRemove.includes(source[i]) === false) {
+        withoutArray.push(source[i]);
+      }
+    }
+  return withoutArray;
+};
 
 
 //TEST CASES
 console.log(without([1, 2, 3], [1])) // => [2, 3]
 console.log(without(["1", "2", "3"], [1, 2, "3"])) // => ["1", "2"]
+
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
 
