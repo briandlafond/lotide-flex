bde531d63b3f5b02c8c08ece9aed90241d5ecf3c
Brian LaFond <brian.d.lafond@gmail.com>
2021-07-26 17:40:53 +0000

    complete

---
+++ countLetters.js
@@ -0,0 +1,30 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const countLetters = function(string) {
+
+  let result = {};
+
+  for (const letter of string) {
+    if (letter !== ' ')
+      if (result[letter]) {
+        result[letter] += 1;
+      } else if (string[letter] !== " ") {
+        result[letter] = 1
+      }
+  }
+  return result;
+
+};
+
+let testString = "brian daniel lafond zzzzz"
+
+console.log(countLetters(testString))
+
