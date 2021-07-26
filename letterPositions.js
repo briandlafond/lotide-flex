b53f7b85a7e0ca25fdcdac99ca1022fea0ce64fb
Brian LaFond <brian.d.lafond@gmail.com>
2021-07-26 18:13:11 +0000

    complete

---
+++ letterPositions.js
@@ -0,0 +1,28 @@
+// const letterPositions = function (string) {
+  
+//   let result = {};
+
+//   return result;
+
+// };
+
+const letterPositions = (string) => {
+  
+  const results = {};
+  
+  for (let i = 0; i < string.length; i++) { // loop through given sentence
+    const letter = string[i];// letter character = index of sentence
+    if (letter !== ' ') {  //bypass whitespace
+      if (results[letter] && Array.isArray(results[letter])) {
+        results[letter].push(i); // adds index position of subsequent character occurence within object
+      } else {
+        results[letter] = [i]; // sets up the the character for first time in the new object w/ index value
+      }
+    }
+  }
+
+  return results;
+
+};
+
+console.log(letterPositions("lighthouse in the house"));
