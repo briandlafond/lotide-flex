f742cb63d2a62558c88b39a819fdf236c137638b
Brian LaFond <brian.d.lafond@gmail.com>
2021-07-29 15:11:14 +0000

    complete

---
+++ findKey.js
@@ -0,0 +1,30 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = (object, callback) => {
+  for (key in object) {
+      const result = callback(object[key]);
+      if (result) {
+          return key;
+      }
+  }
+}
+
+const answer = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri": { stars: 3 },
+  "noma": { stars: 2 },
+  "elBulli": { stars: 3 },
+  "Ora": { stars: 2 },
+  "Akelarre": { stars: 3 }
+}, x => x.stars === 2)
+
+console.log(answer);
+
+assertEqual(answer, 'noma')
